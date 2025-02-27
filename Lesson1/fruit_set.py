82e7e6c8a815bb998cebedb7101752a6433f4940
ajowyang <andrewowyang875@gmail.com>
2025-02-26 18:53:10 -0800

    fruit set exercise

---
+++ Lesson1/fruit_set.py
@@ -0,0 +1,8 @@
+fruits = {'apple', 'banana', 'cherry'}
+fruits.add('grape')
+print(fruits)
+#{'apple', 'banana', 'cherry', 'grape'}
+fruits.add('orange')
+fruits.add('pineapple')
+fruits.add(4)
+print(fruits)
