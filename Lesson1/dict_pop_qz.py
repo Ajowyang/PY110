0fcf096c36e162b1ec82b1070f6cc6315aa0d4ff
ajowyang <andrewowyang875@gmail.com>
2025-02-26 17:33:32 -0800

    completed dictionaries pop quiz

---
+++ Lesson1/dict_pop_qz.py
@@ -0,0 +1,19 @@
+dict1 = {
+     'apple': 'Produce',
+     'carrot': 'Produce',
+     'pear': 'Produce',
+     'broccoli': 'Produce',
+}
+
+dict1['apple'] = 'Fruit'
+dict1['carrot'] = 'Vegetable'
+dict1['pear'] = 'Fruit'
+dict1['broccoli'] = 'Vegetable'
+print(dict1)
+# Pretty printed for clarity
+#{
+#    'apple': 'Fruit',
+#    'carrot': 'Vegetable',
+#    'pear': 'Produce',
+#    'broccoli': 'Produce',
+#}
